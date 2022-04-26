67b71493570e0ddfb83240e3bf9a238c593f60af
kelseybcrow <kelseybcrow@gmail.com>
2022-04-26 16:02:11 -0600

    add Joi validation schema

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 import Input from './common/input';
 
 class LoginForm extends Component {
@@ -9,16 +10,29 @@ class LoginForm extends Component {
         },
     };
 
+    schema = {
+        username: Joi.stringify().required().label('Username'),
+        password: Joi.stringify().required().label('Password'),
+    };
+
     validate = () => {
+        const options = { abortEarly: false };
+        const { error } = Joi.validate(
+            this.state.account,
+            this.schema,
+            options
+        );
+        if (!error) return null;
+
         const errors = {};
-
-        const { account } = this.state;
-        if (account.username.trim() === '')
-            errors.username = 'Username is required.';
-        if (account.password.trim() === '')
-            errors.password = 'Password is required.';
-
-        return Object.keys(errors).length === 0 ? null : errors;
+        for (let item of error.details) errors[item.path[0]] = item.message;
+        return errors;
+    };
+    validateProperty = ({ name, value }) => {
+        const obj = { [name]: value };
+        const schema = { [name]: this.schema[name] };
+        const { error } = Joi.validate(obj, schema);
+        return error ? error.details[0].message : null;
     };
     handleSubmit = (e) => {
         e.preventDefault();
@@ -33,9 +47,14 @@ class LoginForm extends Component {
     };
 
     handleChange = ({ currentTarget: input }) => {
+        const errors = { ...this.state.errors };
+        const errorMessage = this.validateProperty(input);
+        if (errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
         const account = { ...account };
         account[input.name] = input.value;
-        this.setState({ account });
+        this.setState({ account, errors });
     };
     render() {
         const { account, errors } = this.state;
